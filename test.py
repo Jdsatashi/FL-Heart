99aceb9f126fe1a813d34acec2d2c267cdbcd60a
Jdsatashi <vdt1073@gmail.com>
2023-09-18 23:39:27 +0700

    little update

--- test.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-r = redis.Redis.from_url('redis://localhost:6379/0')
-
-try:
-    ping = r.ping()
-    print(f'Connected to Redis Server: {ping}')
-except Exception as e:
-    print(f'Error connecting to Redis: {e}')
